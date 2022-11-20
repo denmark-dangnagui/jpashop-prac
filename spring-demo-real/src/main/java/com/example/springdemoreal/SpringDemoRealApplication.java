27ac1226f8e55debf0537e3fbc7c3f83901b3024
dlrgks88 <dlrgks88@naver.com>
2022-11-20 23:06:17 +0900

    real1

---
+++ spring-demo-real/src/main/java/com/example/springdemoreal/SpringDemoRealApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemoreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoRealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoRealApplication.class, args);
+	}
+
+}
---
+++ spring-demo-real/src/test/java/com/example/springdemoreal/SpringDemoRealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemoreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoRealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
