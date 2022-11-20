091a2707e97364df4f4ff8f1d14cf05b15ea95b4
dlrgks88 <dlrgks88@naver.com>
2022-11-20 22:47:31 +0900

    chapter1

---
+++ shop-spring-demo/src/main/java/com/example/UserDto.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class UserDto {
+
+    private String name;
+    private Integer age;
+
+}
